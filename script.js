f2f8e6bc91b752e41b53621728a810157107175c
radtradcath <vinicius.b.bonafini@gmail.com>
2023-09-11 15:34:28 -0300

    Finish bullet points functionality

--- script.js
+++ script.js
@@ -2,79 +2,75 @@ const btnNext = document.querySelector(".next");
 const btnPrev = document.querySelector(".prev");
 const carousel = document.querySelector(".carousel");
 const images = document.querySelectorAll(".img-container");
-const bulletBtns = document.querySelectorAll('.bullet-points > button');
-const bulletPoints = document.querySelector('.bullet-points');
+const bulletBtns = document.querySelectorAll(".bullet-points > button");
+const bulletPoints = document.querySelector(".bullet-points");
 
 btnNext.addEventListener("click", transformRight);
 btnPrev.addEventListener("click", transformLeft);
-bulletBtns.forEach(btn => {
-  btn.addEventListener('click', changeImage);
+bulletBtns.forEach((btn) => {
+  btn.addEventListener("click", changeImage);
 });
 
 const imgWidth = 500;
 let currentPosition = 0;
-let counter = 1;
 let btnPos;
 
 function changeImage(e) {
-  bulletBtns.forEach(btn => {
-    btn.style = 'background-color: white;';
+  bulletBtns.forEach((btn) => {
+    btn.style = "background-color: white;";
   });
-  e.target.style = 'background-color: yellow;';
+  e.target.style = "background-color: yellow;";
 
-  btnPos = [...bulletBtns].indexOf(e.target) + 1;
+  btnPos = [...bulletBtns].indexOf(e.target);
 
-
-  if (btnPos > counter) {
-    counter = btnPos;
-    console.log(counter);
-    console.log(btnPos);
-    carousel.setAttribute(
-      "style",
-      `transform:  translate(${-(btnPos*imgWidth)}px);`
-    );
-  } else if (btnPos < counter) {
-    counter = btnPos; 
-    console.log(counter);
-    console.log(btnPos)
-    carousel.setAttribute(
-      "style",
-      `transform:  translate(${(btnPos*imgWidth)}px);`
-    );
-  } else {
-    return
-  }
+  carousel.setAttribute(
+    "style",
+    `transform: translate(${-imgWidth * btnPos}px)`
+  );
+  currentPosition = -imgWidth * btnPos;
 }
 
 function transformRight() {
-  if (counter === images.length) {
-    counter = 1;
+  if (currentPosition === -1500) {
     currentPosition = 0;
-
     carousel.setAttribute("style", `transform:  translate(0px);`);
+    colorBullet(currentPosition);
   } else {
     carousel.setAttribute(
       "style",
       `transform:  translate(${currentPosition - imgWidth}px);`
     );
     currentPosition = currentPosition - imgWidth;
-    counter++;
+    colorBullet(currentPosition);
   }
 }
 
-function transformLeft() {
-  if (counter === 1) {
-    counter = 4;
-    currentPosition = -imgWidth * 3;
+function colorBullet(current) {
+  bulletBtns.forEach((btn) => {
+    btn.style = "background-color: white;";
+  });
 
-    carousel.setAttribute("style", `transform:  translate(${-imgWidth * 3}px);`);
+  let arrayOfValues = [0, -500, -1000, -1500];
+
+  let bulletIndex = arrayOfValues.indexOf(current);
+  bulletBtns[bulletIndex].style = 'background-color: yellow;';
+  
+}
+
+function transformLeft() {
+  if (currentPosition === 0) {
+    currentPosition = -imgWidth * 3;
+    colorBullet(currentPosition);
+    carousel.setAttribute(
+      "style",
+      `transform:  translate(${-imgWidth * 3}px);`
+    );
   } else {
     carousel.setAttribute(
       "style",
       `transform:  translate(${currentPosition + imgWidth}px);`
     );
     currentPosition = currentPosition + imgWidth;
-    counter--;
+    colorBullet(currentPosition);
   }
 }
-
